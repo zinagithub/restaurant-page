91d6e8018c7d0d207a81f77a21538aec6990666c
Talaa Zina <talazina@gmail.com>
2019-07-16 19:11:53 +0100

    add some html and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("Hello word")
+import './style.css';
+import Icon from './index.jpeg';
+
+function component() {
+    //const element = document.createElement('div');
+
+
+  // Add the image to our existing div.
+   /*const myIcon = new Image();
+   myIcon.src = Icon;
+   myIcon.width = 
+
+   element.appendChild(myIcon);*/
+   //const elm = document.getElementsByTagName("body");
+   //elm.background = "orange"
+    //return elm;
+  }
+  component()
+  //document.body.appendChild(component());
